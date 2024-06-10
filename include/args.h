7e17af6fb29d98f538cab80db0fdbef1bd170929
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2024-06-11 00:25:28 +0200

    Hide C bindings from C++ interface

--- include/args.h
+++ include/args.h
@@ -2,13 +2,17 @@
 #define ARGS_H
 
 #ifdef __cplusplus
+
 extern "C" {
 namespace args {
 struct Parser;
 typedef Parser args_parser;
+
 #else
+
 struct __Parser;
 typedef struct __Parser args_parser;
+
 #endif
 
 typedef struct {
@@ -19,6 +23,15 @@ typedef struct {
     const char *message;
     int group;
 } args_option_t;
+
+typedef int (*args_parse_f)(int key, const char *arg, args_parser *parser);
+
+typedef struct {
+    const args_option_t *options;
+    const args_parse_f parse;
+    const char *doc;
+    const char *message;
+} args_argp_t;
 
 enum args_option_e {
     ARGS_OPTION_ARG_OPTIONAL = 0x1,
@@ -35,18 +48,13 @@ enum args_key_e {
     ARGS_KEY_ERROR = 0x1000005,
 };
 
-struct args_Parser;
 
-typedef int (*args_parse_f)(int key, const char *arg, args_parser *parser);
-typedef struct {
-    const args_option_t *options;
-    const args_parse_f parse;
-    const char *doc;
-    const char *message;
-} args_argp_t;
+#if !defined __cplusplus || defined WITH_C_BINDINGS
 
 int args_parse(args_argp_t *argp, int argc, char *argv[], void *input);
 void *args_parser_input(args_parser *parser);
+
+#endif
 
 #ifdef __cplusplus
 } // namespace args
